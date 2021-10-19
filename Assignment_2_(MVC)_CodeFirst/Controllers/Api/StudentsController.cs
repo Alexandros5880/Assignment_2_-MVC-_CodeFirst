511b1d8096b636295e3bd8a439649703ad6129fa
Alexandros5880 <Alexpl_15@gmail.com>
2021-10-19 17:25:12 +0300

    Trainers API Controller OK.

---
+++ Assignment_2_(MVC)_CodeFirst/Controllers/Api/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using Assignment_2__MVC__CodeFirst.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2__MVC__CodeFirst.Controllers.Api
+{
+    public class StudentsController : ApiController, IMyController<IHttpActionResult, Student>
+    {
+        public IHttpActionResult Create(Student obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHttpActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHttpActionResult Update(int id, Student obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment_2_(MVC)_CodeFirst/Controllers/Api/TrainersController.cs
@@ -0,0 +1,59 @@
+﻿using Assignment_2__MVC__CodeFirst.Models.Entities;
+using Assignment_2__MVC__CodeFirst.Static;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2__MVC__CodeFirst.Controllers.Api
+{
+    public class TrainersController : ApiController, IMyController<IHttpActionResult, Trainer>
+    {
+        public IHttpActionResult Create(Trainer obj)
+        {
+            Globals.trainerRepo.Add(obj);
+            Globals.DbHundler.Save();
+            return Ok(obj);
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var trainer = Globals.trainerRepo.Get(id);
+            Globals.trainerRepo.Delete(trainer);
+            Globals.DbHundler.Save();
+            return Ok(trainer);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var trainer = Globals.trainerRepo.Get(id);
+            //trainer.Courses = Globals.trainerRepo.GetCourses(trainer.ID);
+            return Ok(trainer);
+        }
+
+        public IHttpActionResult GetAll()
+        {
+            var trainers = Globals.trainerRepo.GetAll();
+            //foreach(var trainer in trainers)
+            //{
+            //    trainer.Courses = Globals.trainerRepo.GetCourses(trainer.ID);
+            //}
+            return Ok(trainers);
+        }
+
+        public IHttpActionResult Update(int id, Trainer obj)
+        {
+            var trainer = Globals.trainerRepo.Get(id);
+            trainer.FirstName = obj.FirstName;
+            trainer.LastName = obj.LastName;
+            trainer.StartDate = obj.StartDate;
+            trainer.School = obj.School;
+            trainer.Courses = obj.Courses;
+            Globals.trainerRepo.Update(trainer);
+            Globals.DbHundler.Save();
+            return Ok(trainer);
+        }
+    }
+}
